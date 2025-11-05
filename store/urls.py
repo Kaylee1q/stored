5c65e6b56adb5eae8b871f8613fcba01fbef8f7b
Kaylee1q <jamie9ko@gmail.com>
2025-11-05 11:38:25 +0500

    login,profile added

--- store/urls.py
+++ store/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),  # connect the products app
+    path('', include('products.urls')),
+    path('users/', include('users.urls')),
 ]
