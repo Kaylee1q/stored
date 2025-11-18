fe38507df08f5c36b3b90da5121e68306a20ff45
Kaylee1q <jamie9ko@gmail.com>
2025-11-18 13:17:07 +0500

    Добавил новые файлы / исправил ошибки

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),          # Home page
-    path('products/', views.products, name='products'),  # Products page
+    path('', views.index, name='index'),          
+    path('products/', views.products, name='products')
 ]
