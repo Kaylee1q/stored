e918e583168b2573712bf48036625a2ff7540eaf
Kaylee1q <jamie9ko@gmail.com>
2025-11-19 11:01:17 +0500

    final edits

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),          
-    path('products/', views.products, name='products')
+    path('products/', views.products_list, name='products'),  # product list page
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),  # detail page
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return render(request, 'storeProducts/index.html')
 
 def products(request):
     return render(request, 'storeProducts/products.html')
+
+def products_list(request):
+    items = Product.objects.all()  # Get all products from DB
+    return render(request, 'storeProducts/products.html', {'products': items})
+
+def product_detail(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    return render(request, 'storeProducts/detail.html', {'product': item})
